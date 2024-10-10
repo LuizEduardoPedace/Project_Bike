604996de4faf11237deb5032bc504af2a048ffce
Luiz Eduardo <pedacededoque@gmail.com>
2024-10-10 14:30:27 -0300

    main, README, gitattributes commit

--- main.sql
+++ main.sql
@@ -1,3 +1,22 @@
+WITH cte AS (
 SELECT *
-FROM cost_table
-LIMIT 100
+FROM bike_share_yr_0
+UNION
+SELECT *
+FROM bike_share_yr_1
+)
+
+SELECT
+    dteday,
+    season,
+    cte.yr,
+    weekday,
+    hr,
+    rider_type,
+    riders,
+    price,
+    COGS,
+    riders*price AS revenue,
+    riders*price - COGS AS profit
+FROM cte
+LEFT JOIN cost_table ON cte.yr = cost_table.yr
